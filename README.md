f6796f3ffe61701dd18ddbe3a108183a1c05d529
HeyAero <aeroyts@icloud.com>
2021-05-06 17:12:15 +0100

    update home view to list books with mock data

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Made by Aaron & Ravil
 
 ### Tasks:
 - [x] Create a Django project with one app, 'library'
-- [ ] Add views and templates for a home and books show page (/ and /books/<id>)
+- [x] Add views and templates for a home and books show page (/ and /books/<id>)
 - [ ] The home page will list all the book titles
 - [ ] The show page will list the chosen book title and author
 - [ ] Show a custom 404 if book is not found
--- library/public/views.py
+++ library/public/views.py
@@ -2,8 +2,15 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
+book_data = { "book_data": [
+    { 'id': 1, 'title': 'Life, the Universe and Everything', 'author': 'Douglas Adams'},
+    { 'id': 2, 'title': 'The Meaning of Liff', 'author': 'Douglas Adams'},
+    { 'id': 3, 'title': 'The No. 1 Ladies\' Detective Agency', 'author': 'Alexander McCall Smith'}
+  ]
+}
+
 def home(req):
-  return render(req, 'public/home.html')
+  return render(req, 'public/home.html', book_data)
 
 def show_book(req):
-  return render(req, 'public/books.html')
+  return render(req, 'public/books.html', book_data)
