0454a89730736e55a1dac8f557554451b937f53f
Ravil Shafiyev <ravil-96@mail.ru>
2021-05-07 11:19:47 +0100

    fixing some errors

--- library/library/urls.py
+++ library/library/urls.py
@@ -16,10 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from user import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='library-admin'),
     path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'), 
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('', include('public.urls')),
 ]
 
--- library/user/forms.py
+++ library/user/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-class UserRegistrationForm(UserCreatingForm):
+class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.CharField(max_length=10)
     last_name = forms.CharField(max_length=10)
