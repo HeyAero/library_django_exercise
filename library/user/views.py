7082e99997d190ad467227fba4e8cd159f057476
Ravil Shafiyev <ravil-96@mail.ru>
2021-05-07 11:02:46 +0100

    adding register view

--- library/user/views.py
+++ library/user/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegistrationForm
+
+def register(req):
+    if req.method == 'POST':
+        form = UserRegistrationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            message.success(req, f'Welcome, {username}!')
+            return redirect('library-home')
 
 # Create your views here.
